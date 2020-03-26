3a3a89359e51f038fbf86fc07350dfe5fc01108c
philip136 <fil13698@gmail.com>
2020-03-26 03:10:31 +0300

    create three table cart|cartItem|order

--- backend/src/magazine/admin.py
+++ backend/src/magazine/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Product, TypeProduct
+from .models import (Product,
+                     TypeProduct,
+                     CartItem,
+                     Cart,
+                     Order)
 
 admin.site.register(Product)
 admin.site.register(TypeProduct)
+admin.site.register(CartItem)
+admin.site.register(Cart)
+admin.site.register(Order)
 
 # Register your models here.
--- backend/src/magazine/models.py
+++ backend/src/magazine/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from sorl.thumbnail import ImageField
+from django.conf import settings
+
 
 class TypeProduct(models.Model):
     """ Type product: books, notepads and etc """
     type = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name = "Тип продукта"
+        verbose_name_plural = "Тип продуктов"
+
     def __str__(self):
         return self.type
     
-
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -18,7 +23,58 @@ class Product(models.Model):
     type = models.ForeignKey(TypeProduct, on_delete=models.CASCADE)
     image = ImageField(upload_to='uploads')
 
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
     def __str__(self):
         return f'{self.name}'
+
+
+ORDER_STATUS_CHOICES = [
+    ("Принят к обработке", "Принят к обработке"),
+    ("Выполняется", "Выполняется"),
+    ("Оплачен", "Оплачен"),
+]
+
+ORDER_TYPE_OF_PURCHASE = [
+    ("Доставка курьером", "Доставка курьером"),
+    ("Самовывоз", "Самовывоз"),
+]
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    count = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        verbose_name = "Продукт корзины"
+        verbose_name_plural = "Продукты корзины"
+    
+
+class Cart(models.Model):
+    products = models.ManyToManyField(CartItem)
+    cart_total = models.DecimalField(max_digits=9,decimal_places=2)
+
+    class Meta:
+        verbose_name = "Корзина"
+        verbose_name_plural = "Корзины"
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    items = models.ManyToManyField(Cart)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=12)
+    date = models.DateTimeField(auto_now_add=True)
+    purchase_type = models.CharField(max_length=30, choices=ORDER_TYPE_OF_PURCHASE, default="Самовывоз")
+    status = models.CharField(max_length=30, choices=ORDER_STATUS_CHOICES)
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
+
+    
 
 
